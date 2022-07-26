b76b02ae834024c1241fa09b19fb4446072c128b
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2022-07-25 23:51:14 -0400

    update correction connexion

--- vues/authentification/formulaireAuthentification.php
+++ vues/authentification/formulaireAuthentification.php
@@ -13,26 +13,13 @@
 <?php if(!isset($_SESSION["utilisateur"])) { ?>
     <h2>Formulaire d'authentification</h2>
     <form method="POST">
-        <label for="utilisateur_login_exemple">Utilisateur (exemple)</label>
-        <input type="text" id="utilisateur_login_exemple" disabled value="sana">
-        <br />
-
-        <label for="mot_de_passe_login_exemple">Mot de passe (exemple)</label>
-        <input type="text" id="mot_de_passe_login_exemple" disabled value="pw">
-        <br />
-
-        <!--
-        <label for="mot_de_passe_crypte_login_exemple">Mot de passe crypte (exemple)</label>
-        <input type="text" id="mot_de_passe_crypte_login_exemple" disabled value="<?= password_hash('pw', PASSWORD_DEFAULT) ?>">
-        <br />
-        -->
-
+       
         <label for="utilisateur_login">Utilisateur</label>
-        <input type="text" id="utilisateur_login" name="utilisateur_login" required>
+        <input type="text" id="utilisateur_login" name="utilisateur_login" value="sana" required>
         <br />
 
         <label for="mot_de_passe_login">Mot de passe</label>
-        <input type="password" id="mot_de_passe_login" name="mot_de_passe_login" required>
+        <input type="password" id="mot_de_passe_login" name="mot_de_passe_login" value="pw" required>
         <br />
 
         <button name="boutonConnexion" type="submit" class="btn btn-primary">Connexion</button>
